8f35525b702c2146f69b9d5345a468f2ea5cedf3
wuhuihao <wuhuihao@qq.com>
2014-03-12 13:16:49 +0800

    test

--- www/js/test.js
+++ www/js/test.js
@@ -9,7 +9,7 @@ $(function () {
                     type: "POST",   //访问WebService使用Post方式请求
                     contentType: "application/json", //WebService 会返回Json类型
                     url: "http:192.168.1.136:15041/ImageWebService.asmx/HelloWorld", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
-                    data: "{}",         //这里是要传递的参数，格式为 data: "{paraName:paraValue}",下面将会看到       
+                    data: null,         //这里是要传递的参数，格式为 data: "{paraName:paraValue}",下面将会看到       
                     dataType: 'json',
                     success: function(result) {     //回调函数，result，返回值
                        $("#txt_title").val(result.d);
