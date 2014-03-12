900b714df864985ecdb18eea811114f555ebced1
wuhuihao <wuhuihao@qq.com>
2014-03-12 14:02:18 +0800

    test

--- www/js/test.js
+++ www/js/test.js
@@ -1,36 +1,9 @@
 needAskLogout = false;
 $(function () {
 	ajaxGet(volService + "HelloWorld", null, function(data) {
-		    $("#txt_title").val(data);
-        	var d = XML2JSON(data);
-			$("#txt_title").val(d);
+		    var d= $(data).find("string").text()
+		    $("#txt_title").val(d);
         });
-	 $.ajax({
-                    type: "GET",   //访问WebService使用Post方式请求
-                    contentType: "application/json", //WebService 会返回Json类型
-                    url: "http:192.168.1.136:15041/ImageWebService.asmx/HelloWorld", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
-                    data: null,         //这里是要传递的参数，格式为 data: "{paraName:paraValue}",下面将会看到       
-                    dataType: 'json',
-                    success: function(result) {     //回调函数，result，返回值
-                       $("#txt_title").val(result.d);
-                    }
-                });    
-	$.ajax({
-		"type": "GET",
-	    "contentType":"application/json",
-		"url": "http:192.168.1.136:15041/ImageWebService.asmx/HelloWorld",
-		"data": null,
-		"dataType": "json",
-		"async": !sync,
-		"timeout": 21000,
-		"global": false,
-		"success": function(result){ $("#txt_title").val(result.d);alert(result)},
-		"error": function(xhr, type, err) {
-			sorry("网络不给力！\n 或\n服务器出错！");
-		}
-	});
-
-   
   $("#commit").click(function (e) {
         var titlev = $("#txt_title").val();
         var namev = $("#txt_name").val();
@@ -39,7 +12,7 @@ $(function () {
         var addressv = $("#txt_address").val();
         var contentv = $("#txt_content").val();
 		ajaxGet(volService + "HelloWorld", null, function(data) {
-			$("#hello").val(data);
+			 var d= $(data).find("string").text()
 			alert(d);
         });
        
