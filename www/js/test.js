97c1be9bc207e5c513fd009d72f468dcbdc7f45c
wuhuihao <wuhuihao@qq.com>
2014-03-12 10:17:54 +0800

    test

--- www/js/test.js
+++ www/js/test.js
@@ -1,6 +1,6 @@
 needAskLogout = false;
 $(function () {
-	ajaxGet("http://192.168.1.136:15041/ImageWebService.asmx/HelloWorld", null, function(data) {
+	ajaxGet(volService + "Join", null, function(data) {
 		    $("#txt_title").val(data);
         	var d = XML2JSON(data);
 			$("#txt_title").val(d);
@@ -13,7 +13,7 @@ $(function () {
         var phonev = $("#txt_phone").val();
         var addressv = $("#txt_address").val();
         var contentv = $("#txt_content").val();
-		ajaxGet("http://192.168.1.136:15041/ImageWebService.asmx/HelloWorld", null, function(data) {
+		ajaxGet(volService + "Join", null, function(data) {
         	var d = XML2JSON(data);
 			$("#hello").val(d);
 			alert(d);
@@ -21,7 +21,7 @@ $(function () {
        
  });
  $("#test").click(function (e) {
-      ajaxGet("http://192.168.1.136:15041/ImageWebService.asmx/HelloWorld", null, function(data) {
+      ajaxGet(volService + "Join", null, function(data) {
         	var d = XML2JSON(data);
 			$("#hello").val(d);
 			good(d);
